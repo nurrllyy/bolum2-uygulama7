23e1b2658295e3eeac819047eebc65e08289b122
nurrllyy <114949657+nurrllyy@users.noreply.github.com>
2023-09-26 10:25:01 +0300

    Add files via upload

---
+++ bolum2-uyglama7/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum2_uyglama7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int deger1 = Convert.ToInt32(textBox1.Text);
+            int deger2 = Convert.ToInt32(textBox2.Text);
+            int sonuc;
+            if (radioButton1.Checked == true)
+            {
+                label3.Text = (deger1+deger2).ToString();
+            }
+            else
+            {
+                label3.Text=(deger1*deger2).ToString();
+            }
+        }
+    }
+}
